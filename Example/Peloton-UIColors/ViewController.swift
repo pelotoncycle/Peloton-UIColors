8b44b4dff978669e6359767612661e86e0de48de
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2016-07-20 14:38:34 -0400

    Peloton UIColors

--- Example/Peloton-UIColors/ViewController.swift
+++ Example/Peloton-UIColors/ViewController.swift
@@ -7,18 +7,19 @@
 //
 
 import UIKit
+import Peloton_UIColors
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = UIColor.pelotonSuperRed()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
 }
 
