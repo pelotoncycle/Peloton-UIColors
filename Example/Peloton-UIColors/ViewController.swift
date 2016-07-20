5422518e14ecc90f35272bbd0dc77c8659a60e29
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2016-07-20 14:31:47 -0400

    initial project setup

---
+++ Example/Peloton-UIColors/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Peloton-UIColors
+//
+//  Created by Ryan Fitzgerald on 07/20/2016.
+//  Copyright (c) 2016 Ryan Fitzgerald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
